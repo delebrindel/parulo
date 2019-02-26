31810ababc27f74485a77ae19225c546397d89b8
Cristobal Torres <delebrindel@gmail.com>
2019-02-25 20:06:07 -0600

    Binds con Vue funcionando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,54 @@
-var wsUri = "ws://localhost:8080";
+var wsUri = "ws://192.168.100.3:8080";
 var output;
 var person;
 
-new Vue({
-    el: '#app',
-    data: {
-      person: "",
-      msg: "",
-      output: []
+var vm = new Vue({
+  el: '#app',
+  data: {
+    person: "",
+    message: "",
+    output: []
+  },
+  mounted: function () {
+    this.person = prompt("Por favor ingresa tu nombre", "User");
+    this.output.push(this.person + " ha iniciado sesión!");
+    this.init();
+  },
+  methods: {
+    init: function () {
+      this.testWebSocket();
     },
-    mounted: function () {
-      this.person = prompt("Por favor ingresa tu nombre", "User");
-      this.output.push( this.person + " ha iniciado sesión!");
+    testWebSocket: function()
+    {
+      websocket = new WebSocket(wsUri);
+      websocket.onopen = function(evt) { vm.onOpen(evt) };
+      websocket.onclose = function(evt) { vm.onClose(evt) };
+      websocket.onmessage = function(evt) { vm.onMessage(evt) };
+      websocket.onerror = function(evt) { vm.onError(evt) };
+    },
+    onOpen: function(evt)
+    {
+      this.output.push({type: "r-out", msg:"Bienvenido/a "+this.person});
+      websocket.send(this.person + " ha iniciado sesión");
+    },
+    onClose: function(evt)
+    {
+      this.output.push({type: "r-er", msg: evt.data});
+    },
+    onMessage: function(evt)
+    {
+      this.output.push({type: "r-in", msg: evt.data});
+    },
+    doSend: function()
+    {
+      this.output.push({type: "r-out", msg:this.person+": " + this.message});
+      websocket.send(this.person+": " + this.message);
+      this.message="";
+    },
+    
+    onError: function(evt)
+    {
+      this.output.push({type: "r-er", msg:'ERROR ' + evt.data});
     }
+  }
 })
-/*
-function init()
-{
-  testWebSocket();
-}
-
-function testWebSocket()
-{
-  websocket = new WebSocket(wsUri);
-  websocket.onopen = function(evt) { onOpen(evt) };
-  websocket.onclose = function(evt) { onClose(evt) };
-  websocket.onmessage = function(evt) { onMessage(evt) };
-  websocket.onerror = function(evt) { onError(evt) };
-}
-
-function onOpen(evt)
-{
-  writeToScreen("CONNECTED");
-  doSend("WebSocket rocks");
-}
-
-function onClose(evt)
-{
-  writeToScreen("DISCONNECTED");
-}
-
-function onMessage(evt)
-{
-  writeToScreen('<span style="color: blue;">' + evt.data+'</span>');
-  //websocket.close();
-}
-
-function onError(evt)
-{
-  writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-}
-
-function doSend()
-{
-  let message = document.querySelector("#message").value; // Things you want to do.
-  writeToScreen(person+": " + message);
-  websocket.send(person+": " + message);
-  document.querySelector("#message").value = ""; // Things you want to do.
-}
-
-function writeToScreen(message)
-{
-  var pre = document.createElement("p");
-  pre.style.wordWrap = "break-word";
-  pre.innerHTML = message;
-  output.appendChild(pre);
-}
-
-window.addEventListener("load", init, false);
-*/
--- chat-client.php
+++ chat-client.php
@@ -3,12 +3,29 @@
 <head>
     <meta charset="utf-8" />
     <title>WebSocket Test</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
+    <link rel="stylesheet" href="assets/css/app.css">
 </head>
+
 <body>
-    <div id="app">
-        <div id="chat">
-            <div class="response" v-for="(item, index) in output" :key="index" v-text="item">
+    <div class="container">
+        <div id="app" class="section">
+            <h1 class="title">Parulo - Una prueba de chat</h1>
+            <div class="columns is-desktop">
+                <div class="column is-one-third">
+                    <div class="field">
+                        <div class="control">
+                            <input type="text" class="input is-info" v-model="message" @keyup.enter="doSend"
+                                placeholder="Mensaje a enviar">
+                        </div>
+                    </div>
+                </div>
+                <div id="chat" class="column is-two-thirds">
+                    <div v-for="(item, index) in output" :key="index" v-text="item.msg" :class="item.type"></div>
+                </div>
+            </div>
         </div>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <script src="assets/js/app.js"></script>
