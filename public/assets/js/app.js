5ab0a2370a4e026083124c8e8f8d0760bb3ae0b3
Cristobal Torres <delebrindel@gmail.com>
2019-02-26 17:58:03 -0600

    Updated URL

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,7 +5,7 @@ var vm = new Vue({
         person: "",
         message: "",
         output: [],
-        websocket : io.connect('https://limitless-refuge-67226.herokuapp.com/', { 'forceNew': true })
+        websocket : io.connect('https://parulo-app.herokuapp.com/', { 'forceNew': true })
     },
     mounted: function () {
         this.person = prompt("Por favor ingresa tu nombre", "User");
