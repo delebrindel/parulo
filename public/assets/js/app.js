66476d1960e05444265d84b7722e854994d084a0
Cristóbal Torres <delebrindel@gmail.com>
2019-02-26 14:54:51 -0600

    Rebuilt with node

---
+++ public/assets/js/app.js
@@ -0,0 +1,40 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        person: "",
+        message: "",
+        output: [],
+        websocket : io.connect('http://localhost:8080', { 'forceNew': true })
+    },
+    mounted: function () {
+        this.person = prompt("Por favor ingresa tu nombre", "User");
+        this.startWebSocket();
+    },
+    methods: {
+        startWebSocket: function()
+        {
+            this.websocket.on('messages', function(data) { vm.onMessage(data); });
+            this.connect();
+        },
+        connect: function (){
+            this.websocket.emit('new-connection', this.person);
+        },
+        onMessage: function(data){
+            if(data.author != this.person){
+                this.output.push({author: data.author, message: data.text, type: "r-out"})
+            } 
+            else{
+                this.output.push({author: data.author, message: data.text, type: "r-in"})
+            }
+        },
+        doSend: function(e) {
+            var message = {
+                author: this.person,
+                text: this.message
+            };
+            this.message="";
+            this.websocket.emit('new-message', message);
+            return false;
+        }
+    }
+})
---
+++ server/main.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var messages = [];
+
+app.use(express.static('./../public'));
+
+app.get('/hello', function(req, res) {
+  res.status(200).send("Hello World!");
+});
+
+io.on('connection', function(socket) {
+    socket.emit({author: "Sistema "}, messages);
+    socket.on('new-message', function(data) {
+        io.sockets.emit('messages', data);
+    });
+    socket.on('new-connection', function(person) {
+      io.sockets.emit('messages', {author: "Sistema", text: person + " ha iniciado sesión", type: "r-sys"});
+  });
+});
+
+server.listen(8080, function() {
+  console.log("Servidor corriendo en http://localhost:8080");
+});
